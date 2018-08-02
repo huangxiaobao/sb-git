7aeedb5720d316ae73489b92e3d18fa7b9aacbf7
黄晓宝 <huang.xiaobao@trs.com.cn>
2018-08-02 15:44:56 +0800

    测试

---
+++ src/main/java/com/hxb/sbgit/SbGitApplication.java
@@ -0,0 +1,12 @@
+package com.hxb.sbgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGitApplication.class, args);
+	}
+}
